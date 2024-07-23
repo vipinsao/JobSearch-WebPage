72a8cd217262fb4f9bea6d5992ac9c0081e1f753
Vipin Sao <68950232+vipinsao@users.noreply.github.com>
2024-07-23 02:43:10 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_worl():
-  return "Hello, Vipin"
+def hello_world():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
